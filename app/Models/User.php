d5f6cce4701cf3b3d43312bfca37bea1a3ece651
Cristhian Lunardi <criscjl14@gmail.com>
2021-01-12 09:59:50 -0400

    Setting up database config (MongoDB) Dependency Model created Using factory and seeder to populate the database

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable
+class User extends \Jenssegers\Mongodb\Eloquent\Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
-    use HasFactory, Notifiable;
+    use Authenticatable, Authorizable, CanResetPassword, Notifiable, HasFactory;
+    //use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
