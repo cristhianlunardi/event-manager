ed4e2cf0fb289c504ae9fe1ed50dcaabca91a529
Carlos Callañaupa <ccallanaupa@auction.com>
2021-07-11 20:55:19 -0400

    fixed: birthday format on register service

--- app/Http/Requests/User/RegisterUserRequest.php
+++ app/Http/Requests/User/RegisterUserRequest.php
@@ -27,7 +27,7 @@ class RegisterUserRequest extends FormRequest
             'email'  => 'bail|required|email|unique:users,email',
             'password'  => 'required|min:6|confirmed',
             'fullName'  => 'required',
-            'birthday'  => 'required|date',
+            'birthday'  => 'required|date_format:d/m/Y',
             'dependency'  => 'sometimes|exists:dependencies,key',
             'rol'  => 'required',
         ];
